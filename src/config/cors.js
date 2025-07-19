6d44d0a08c96a412bdf6ccdaea15d83d4057b8ef
hyson0807 <simsgood0807@gmail.com>
2025-07-20 01:30:08 +0900

    버전 1 업로드

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,6 +1,6 @@
 const corsOptions = {
     origin: process.env.NODE_ENV === 'production'
-        ? ['kgencyserver-production.up.railway.app'] // 프로덕션 도메인으로 변경 필요
+        ? ['https://kgencyserver-production.up.railway.app'] // 프로덕션 도메인으로 변경 필요
         : ['http://localhost:3000', 'http://localhost:8081'], // 개발 환경
     credentials: true,
     optionsSuccessStatus: 200
