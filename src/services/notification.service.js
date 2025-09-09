da4381a78471c298df0e51706d1ee6e7509a1186
hyson0807 <simsgood0807@gmail.com>
2025-09-09 22:48:28 +0900

    불필요한 알림 삭제

--- src/services/notification.service.js
+++ src/services/notification.service.js
@@ -71,95 +71,9 @@ class NotificationService {
     }
   }
 
-  /**
-   * Send interview proposal notification
-   * @param {string} userId - User ID who will receive the notification
-   * @param {string} companyName - Name of the company
-   * @param {string} jobTitle - Job title
-   * @param {string} applicationId - Application ID for navigation
-   */
-  async sendInterviewProposalNotification(userId, companyName, jobTitle, applicationId) {
-    const title = '면접 제안이 도착했습니다!';
-    const body = `${companyName}에서 ${jobTitle} 포지션 면접을 제안했습니다.`;
-    const data = {
-      type: 'interview_proposal',
-      applicationId,
-      companyName,
-      jobTitle,
-    };
 
-    return await this.sendToUser(userId, title, body, data);
-  }
 
-  /**
-   * Send application status update notification
-   * @param {string} userId - User ID
-   * @param {string} companyName - Company name
-   * @param {string} status - New status
-   * @param {string} applicationId - Application ID
-   */
-  async sendApplicationStatusNotification(userId, companyName, status, applicationId) {
-    const statusMessages = {
-      accepted: '지원이 승인되었습니다!',
-      rejected: '지원이 거절되었습니다.',
-      pending: '지원이 검토 중입니다.',
-    };
 
-    const title = '지원 상태 업데이트';
-    const body = `${companyName} - ${statusMessages[status] || '상태가 변경되었습니다.'}`;
-    const data = {
-      type: 'application_status',
-      applicationId,
-      status,
-      companyName,
-    };
-
-    return await this.sendToUser(userId, title, body, data);
-  }
-
-  /**
-   * Send interview schedule confirmation notification to company
-   * @param {string} companyId - Company ID who will receive the notification
-   * @param {string} userName - Name of the user who confirmed the interview
-   * @param {string} jobTitle - Job title
-   * @param {string} interviewDate - Interview date and time
-   * @param {string} applicationId - Application ID for navigation
-   */
-  async sendInterviewScheduleConfirmationToCompany(companyId, userName, jobTitle, interviewDate, applicationId) {
-    const title = '면접 일정이 확정되었습니다!';
-    const body = `${userName}님이 ${jobTitle} 포지션 면접 일정을 확정했습니다. (${interviewDate})`;
-    const data = {
-      type: 'interview_schedule_confirmed',
-      applicationId,
-      userName,
-      jobTitle,
-      interviewDate,
-    };
-
-    return await this.sendToUser(companyId, title, body, data);
-  }
-
-  /**
-   * Send interview cancellation notification to user
-   * @param {string} userId - User ID who will receive the notification
-   * @param {string} companyName - Name of the company that cancelled
-   * @param {string} jobTitle - Job title
-   * @param {string} interviewDate - Interview date and time that was cancelled
-   * @param {string} applicationId - Application ID for navigation
-   */
-  async sendInterviewCancellationNotification(userId, companyName, jobTitle, interviewDate, applicationId) {
-    const title = '면접이 취소되었습니다';
-    const body = `${companyName}에서 ${jobTitle} 포지션 면접이 취소되었습니다. (${interviewDate})`;
-    const data = {
-      type: 'interview_cancelled',
-      applicationId,
-      companyName,
-      jobTitle,
-      interviewDate,
-    };
-
-    return await this.sendToUser(userId, title, body, data);
-  }
 
   /**
    * Send new application notification to company
@@ -184,88 +98,9 @@ class NotificationService {
     return await this.sendToUser(companyId, title, body, data);
   }
 
-  /**
-   * Send interview request acceptance notification to company
-   * @param {string} companyId - Company ID who will receive the notification
-   * @param {string} userName - Name of the user who accepted
-   * @param {string} jobTitle - Job title
-   * @param {string} requestType - Type of request ('job_posting', 'home_user')
-   * @param {string} applicationId - Application ID for navigation
-   */
-  async sendInterviewRequestAcceptanceNotification(companyId, userName, jobTitle, requestType, applicationId) {
-    const sourceText = requestType === 'job_posting' ? '공고 지원자' : '홈화면 유저';
-    const title = '면접 요청이 수락되었습니다!';
-    const body = `${userName}님이 ${jobTitle} 포지션 면접 요청을 수락했습니다. (${sourceText})`;
-    const data = {
-      type: 'interview_request_accepted',
-      applicationId,
-      userName,
-      jobTitle,
-      requestType,
-    };
 
-    return await this.sendToUser(companyId, title, body, data);
-  }
 
-  /**
-   * Send job posting interview proposal notification to user
-   * @param {string} userId - User ID who will receive the notification
-   * @param {string} companyName - Name of the company
-   * @param {string} jobTitle - Job title
-   * @param {string} applicationId - Application ID for navigation
-   */
-  async sendJobPostingInterviewProposalNotification(userId, companyName, jobTitle, applicationId) {
-    const title = '지원한 공고에서 면접 제안이 왔습니다!';
-    const body = `${companyName}에서 ${jobTitle} 포지션 면접을 제안했습니다.`;
-    const data = {
-      type: 'job_posting_interview_proposal',
-      applicationId,
-      companyName,
-      jobTitle,
-    };
 
-    return await this.sendToUser(userId, title, body, data);
-  }
-
-  /**
-   * Send instant interview cancellation notification to user
-   * @param {string} userId - User ID who will receive the notification
-   * @param {string} companyName - Name of the company
-   * @param {string} jobTitle - Job title
-   * @param {string} applicationId - Application ID for navigation
-   */
-  async sendInstantInterviewCancellationNotification(userId, companyName, jobTitle, applicationId) {
-    const title = '즉시면접이 취소되었습니다';
-    const body = `${companyName}에서 ${jobTitle} 포지션 즉시면접이 취소되었습니다.`;
-    const data = {
-      type: 'instant_interview_cancelled',
-      applicationId,
-      companyName,
-      jobTitle,
-    };
-
-    return await this.sendToUser(userId, title, body, data);
-  }
-
-  /**
-   * Send regular application cancellation notification to user
-   * @param {string} userId - User ID who will receive the notification
-   * @param {string} companyName - Name of the company
-   * @param {string} jobTitle - Job title
-   * @param {string} applicationId - Application ID for navigation
-   */
-  async sendRegularApplicationCancellationNotification(userId, companyName, jobTitle, applicationId) {
-    const title = '지원이 취소되었습니다';
-    const body = `${companyName}에서 ${jobTitle} 포지션 지원이 취소되었습니다.`;
-    const data = {
-      type: 'regular_application_cancelled',
-      applicationId,
-      companyName,
-      jobTitle,
-    };
-
-    return await this.sendToUser(userId, title, body, data);
-  }
 
   /**
    * Send chat message notification to user with badge count
